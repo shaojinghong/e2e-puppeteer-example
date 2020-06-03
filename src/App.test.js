45ff0a1b68d663d255562375507596524d0f19f4
shaojinghong <shaojinghong@360.cn>
2020-06-03 16:52:49 +0800

    测试用例

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,64 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import puppeteer from "puppeteer";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let browser;
+let page;
+
+describe("Counter test", () => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+  });
+
+  // 1
+  it("renders counter", async () => {
+    await page.waitForSelector(".header");
+
+    const header = await page.$eval(".header", (e) => e.innerHTML);
+    expect(header).toBe("Counter");
+
+    await page.screenshot({ path: "e2e-snapshots/renders-counter.png" });
+  });
+
+  // 2
+  it("sets initial state to zero", async () => {
+    await page.waitForSelector(".counter-app");
+
+    const count = await page.$eval(".count", (e) => e.innerHTML);
+    expect(count).toBe("0");
+
+    await page.screenshot({ path: "e2e-snapshots/counter-zero.png" });
+  });
+
+  // 3
+  it("increments counter by 1", async () => {
+    await page.waitForSelector(".counter-app");
+
+    await page.click(".increment");
+    const count = await page.$eval(".count", (e) => e.innerHTML);
+    expect(count).toBe("1");
+
+    await page.screenshot({
+      path: "e2e-snapshots/increments-counter-by-1.png",
+    });
+  });
+
+  // 4
+  it("decrements counter by 1", async () => {
+    await page.waitForSelector(".counter-app");
+
+    await page.click(".decrement");
+    const count = await page.$eval(".count", (e) => e.innerHTML);
+    expect(count).toBe("0");
+
+    await page.screenshot({
+      path: "e2e-snapshots/decrements-counter-by-1.png",
+    });
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
 });
